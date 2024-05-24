3441865be2b7df197ba837ace0d2e4a2cca8fa66
Lost-Crow <ruhal.appli@gmail.com>
2024-05-25 00:59:41 +0100

    just code of milestone_4

--- milestone_4.py
+++ milestone_4.py
@@ -2,44 +2,7 @@
 import random
 
 class Hangman:
-    """
-     A Hangman Game that asks the user for a letter and checks if it is in the word.
-    It starts with a default number of lives and a random word from the word_list.
-
     
-    Parameters:
-    ----------
-    word_list: list
-        List of words to be used in the game
-    num_lives: int
-        Number of lives the player has
-    
-    Attributes:
-    ----------
-    num_lives: int
-        The number of lives the player has
-    word: str
-        The word to be guessed picked randomly from the word_list
-    word_guessed: list
-        A list of the letters of the word, with '_' for each letter not yet guessed
-        For example, if the word is 'apple', the word_guessed list would be ['_', '_', '_', '_', '_']
-        If the player guesses 'a', the list would be ['a', '_', '_', '_', '_']
-    num_letters: int
-        The number of UNIQUE letters in the word that have not been guessed yet
-    list_of_guesses: list
-        A list of the letters that have already been tried
-
-    Methods:
-    -------
-    check_guess(guess)
-        Checks if the letter is in the word.
-    ask_letter()
-        Asks the user for a letter.
-    
-    
-    """
-
-
     def __init__(self, word_list: list[str], num_lives=5) -> None:
             # Initializing the attributes as indicated in the docstring
             self.num_lives = num_lives
@@ -51,19 +14,6 @@ class Hangman:
             self.list_of_guesses = []
 
     def __check_guess(self, guess) -> None: 
-        """
-        Checks if the letter is in the word.
-        If it is, it replaces the '_' in the word_guessed list with the letter, thus reducing the num_letters variable by 1.
-        If it is not, it reduces the number of lives by 1.
-
-        Parameters:
-        ----------
-        (' __ ' = private methods cannot be accessed directly from outside the class, but used within class/subclass)
-
-        guess: str
-            The letter to be checked
-
-        """
         guess.lower() # converts capitals in word to lowercase
         if guess in self.word:
             print(f"Good guess!: {guess} is in secret word {self.word}")
@@ -78,17 +28,6 @@ class Hangman:
         print(self.word_guessed)
     
     def ask_letter(self):
-        """
-
-        Asks the user for a letter and checks two things:
-        1. If the character is a single character and within the alphabet
-        2. If the letter has already been tried and within the list of guesses
-        3. Appends the letter into the list of guesses tried
-        If it passes both checks, it calls the check_letter method.
-        
-        
-        """
-
         while True:
             letter = input("Enter your letter: ")
             if len(letter) != 1 or not letter.isalpha():
